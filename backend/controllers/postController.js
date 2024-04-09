ce895d16f383259ffbe6871194f66ce3af7c4206
Stella Ndjikessi <stelladiana@live.fr>
2024-04-09 17:38:37 +0200

    Controller to get all the posts

---
+++ backend/controllers/postController.js
@@ -0,0 +1,46 @@
+const Post = require('../models/postModel');
+
+// Get all posts
+const getPosts = async (req, res) => {
+  const posts = await Post.find({}).sort({createdAt: -1});
+
+  res.status(200).json(posts);
+}
+
+// Get a single post
+const getPost = async (req, res) => {
+  const { id } = req.params;
+
+  if(!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({error: 'No such post'});
+  }
+
+  const post = await Post.findById(id);
+
+  if(!post) {
+    return res.status(400).json({error: 'No such post'});
+  };
+
+  res.status(200).json(post);
+}
+
+// Create a new post
+const createPost = async (req, res) => {
+  const { content } = req.body;
+
+  // Add post to db
+  try {
+    const post = await Post.create({content});
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(400).json({error: error.message})
+  }
+}
+
+// Delete a post
+
+module.exports = {
+  createPost,
+  getPosts,
+  getPost
+}
--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,29 +1,25 @@
 const express = require('express');
+const { createPost, getPost, getPosts } = require('../controllers/postController');
+
 const router = express.Router();
 
 // GET all posts
-router.get('/', (req, res) => {
-  res.json('GET all the posts')
-});
+router.get('/', getPosts);
 
 // GET a single post
-router.get('/:id', (req, res) => {
-  res.json('GET a single post');
-})
+router.get('/:id', getPost); 
 
 // POST a new post
-router.post('/', (req, res) => {
-  res.json('POST a new post')
-})
+router.post('/', createPost);
 
 // DELETE a post
 router.delete('/:id', (req, res) => {
-  res.json('DELETE a post')
+  res.json('DELETE a post');
 });
 
 // UPDATE
 router.patch('/:id', (req, res) => {
-  res.json('UPDATE a post')
+  res.json('UPDATE a post');
 })
 
 
