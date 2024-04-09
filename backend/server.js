bc6ebcd43b3985483ebb9e86ed6736adeb2de8a6
Stella Ndjikessi <stelladiana@live.fr>
2024-04-09 16:32:39 +0200

    Basic posts routes

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,24 @@
 require('dotenv').config();
 const express = require('express');
 
+const postsRoutes = require('./routes/posts');
+
 // Express app
 const app = express();
+
+// Middleware
+app.use(express.json());
 
 app.use((req, res, next) => {
   console.log(req.path, req.method)
   next()
-})
+});
 
 // Routes
-app.get('/', (req, res) => {
-  res.json('Welcome to your app');
-})
+app.use('/api/posts', postsRoutes);
 
 // Server running
 app.listen(process.env.PORT, () => {
   console.log('Server is running on port:', process.env.PORT);
-})
+});
+ 
