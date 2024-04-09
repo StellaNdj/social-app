48e7b6bdf1151ca0882425fdbaf7956eea1c672f
Stella Ndjikessi <stelladiana@live.fr>
2024-04-09 18:27:25 +0200

    Fetching posts from server

--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Homepage = () => {
-  return(<h2>Homepage</h2>)
+  const [posts, setPosts] = useState<any>(null);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch('/api/posts');
+      const json = await response.json()
+
+      if (response.ok) {
+        setPosts(json);
+      }
+    }
+    fetchPosts()
+  }, [])
+
+  console.log(posts);
+
+  return(
+    <div>
+      <div>
+        {posts !== null && posts.map((post) => {
+          return <p key={post._id}>{post.content}</p>
+        })}
+      </div>
+    </div>
+)
 };
 
 export default Homepage;
