38b2c26cf6d12a76812b7816ebaf2cdcfd038816
Stella Ndjikessi <stelladiana@live.fr>
2024-04-10 08:49:25 +0200

    Signup static logic added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,5 @@
-const User = require('../models/userModel');
+const {UserSignup, UserLogin} = require('../models/userModel');
+
 
 // Login user
 const loginUser = async (req, res) => {
@@ -8,7 +9,14 @@ const loginUser = async (req, res) => {
 // Sign up user
 
 const signupUser = async (req, res) => {
-  res.json('Signup used');
+  const { email, password, firstName, lastName, username } = req.body;
+
+  try {
+    const user = await UserSignup.signup(email, password, firstName, lastName, username);
+    res.status(200).json({email, user})
+  } catch (error) {
+    res.status(400).json({error: error.message})
+  }
 };
 
 module.exports = { loginUser, signupUser }
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const validator = require('validator');
 
 const Schema = mongoose.Schema;
 
+
+// Signup schema
 const signupSchema = new Schema({
   email: {
     type: String,
@@ -39,7 +43,39 @@ const loginSchema = new Schema({
   }
 });
 
+// Static signup method
+signupSchema.statics.signup = async function(email, password, lastName, firstName, username) {
+
+  // Validation
+  if(!email || !password || !lastName || !firstName || !username ) {
+    throw Error('All the fields must be filled');
+  };
+
+  if(!validator.isEmail(email)) {
+    throw Error('Email is not valid');
+  };
+
+  if(!validator.isStrongPassword(password)) {
+    throw Error('Password is not strong enough, must contain capitals, number and speical character');
+  }
+
+  // Check that email is not in use
+  const exists = await this.findOne({ email });
+
+  if(exists) {
+    throw Error('Email already in use');
+  }
+
+  // Hash the password for safety
+  const salt = await bcrypt.genSalt(10)
+  const hash = await bcrypt.hash(password, salt);
+
+  const user = await this.create({ email, password: hash, lastName, firstName, username});
+
+  return user;
+}
+
 module.exports = {
-  UserSignup: mongoose.model('UserSignUp', signupSchema),
+  UserSignup: mongoose.model('UserSignup', signupSchema),
   UserLogin: mongoose.model('UserLogin', loginSchema)
 };
